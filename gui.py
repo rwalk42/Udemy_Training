7c5bd6cc99fef7032874f1b02f4694819fc19eab
Robert <robert@intenseit.co.uk>
2023-01-20 12:16:28 +0000

    GUI program updated with Edit function - Day 17

--- gui.py
+++ gui.py
@@ -2,11 +2,51 @@ import Functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a To-Do")
-input_box = sg.InputText(tooltip="Enter To-Do")
+input_box = sg.InputText(tooltip="Enter To-Do", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=Functions.get_todos(),
+                      key='todos',
+                      enable_events=True,
+                      size=[45, 10])
+edit_button = sg.Button("Edit")
 
-window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
-window.read()
+
+window = sg.Window("My To-Do App",
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   font=('Helvitics',20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    print(values['todos'])
+    match event:
+        case "Add":
+            todos = Functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            Functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            new_todo = new_todo + "\n"
+
+            todos = Functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            Functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+        case sg.WIN_CLOSED:
+            break
+
+
+
 window.close()
+
+
+
 
 
